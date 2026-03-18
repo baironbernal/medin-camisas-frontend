7853f2303a50622f6e6997d08e6a611a547a5314
Bairon Bernal <baironbernal263@gmail.com>
2026-03-18 10:51:19 -0500

    update

--- app/(shop)/(detail)/producto/[slug]/page.tsx
+++ app/(shop)/(detail)/producto/[slug]/page.tsx
@@ -1,13 +1,13 @@
 import { getProduct } from "@/app/services/products";
 import { notFound } from "next/navigation";
 import { ProductDetail } from "@/types/product-detail";
-import { WrapperDetail } from "@/app/(shop)/components";
+import ProductDetailWithDiscount from "@/app/(shop)/components/detail/ProductDetailWithDiscount";
+
 
 export default async function Page ({params}: {params: { slug: string }}) {
     const { slug } = await params;
 
     const product = await getProduct<ProductDetail>(slug);
-
 
     if (!product) {
       notFound()
@@ -15,7 +15,7 @@ export default async function Page ({params}: {params: { slug: string }}) {
     
   return (
     <main className="flex items-justify justify-center py-12 bg-beige">
-      <WrapperDetail data={product.data} />
+      <ProductDetailWithDiscount data={product.data} />
     </main> 
   )
 }
